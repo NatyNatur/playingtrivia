b06363a0923178bc3d387e5cddc789bd13817e6e
NatyNatur <naticcg@gmail.com>
2018-02-17 23:52:48 -0300

    subiendo opciones de trivia

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-onst btnStart = document.getElementById('btnStart');
+const btnStart = document.getElementById('btnStart');
 const urlCateg = `https://opentdb.com/api_category.php`;
 let i;
 let correctAnswer;
@@ -6,7 +6,7 @@ let correctAnswersCounter;
 
 btnStart.addEventListener('click', function(event) {
   getAnyTrivia();
-})
+});
 
 function getTrivia() {
   fetch(urlCateg)
@@ -24,21 +24,21 @@ function getTrivia() {
       let music = `<div class="col s3">
         <a class="waves-effect waves-light btn pink btnCat" onclick=getMusic();>${data.trivia_categories[3].name}</a>
       </div>`;
-      let videogames =`<div class="col s3">
+      let videogames = `<div class="col s3">
         <a class="waves-effect waves-light btn pink btnCat" onclick=getVideogames();>${data.trivia_categories[5].name}</a>
       </div>`;
-      let anime =`<div class="col s3">
+      let anime = `<div class="col s3">
         <a class="waves-effect waves-light btn pink btnCat" onclick=getAnime();>${data.trivia_categories[6].name}</a>
       </div>`;
 
       html.push(generalK + music + videogames + anime);
 
-    document.getElementById('showingCategories').innerHTML = html;
+      document.getElementById('showingCategories').innerHTML = html;
     })
     .catch(function(error) {
       console.log(error);
     });
-    btnStart.remove();
+  btnStart.remove();
 }
 
 
@@ -55,3 +55,125 @@ function getAnyTrivia() {
   btnStart.remove();
 }
 
+function trueOrFalse() {
+  fetch(`https://opentdb.com/api.php?amount=10&type=boolean`).
+    then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      let results = data.results;
+      let i = 0;
+      let question = results[i].question;
+      let category = results[i].category;
+      let difficulty = results[i].difficulty;
+      correctAnswer = results[i].correct_answer;
+      let wrongAnswer = results[i].incorrect_answers[i];
+      correctAnswersCounter = 0;
+
+      let questionsCont = `<div class="col s12 m8 offset-m2 center-align">
+        <div class="card grey darken-1">
+          <div class="card-content white-text">
+            <span class="card-title">
+              <span class="pink-text text-accent-1">${category}</span> |
+              <span class="teal-text text-lighten-1">${difficulty} </span> |
+               Question number ${i + 1}</span>
+            <p class="questions">${question}</p>
+          </div>
+          <div class="card-action">
+            <a class="waves-effect waves-light btn pink" onclick="checkingAnswerTF();" id="True">true</a>
+            <a class="waves-effect waves-light btn pink" onclick="checkingAnswerTF();" id="False">false</a>
+          </div>
+        </div>
+      </div>`;
+      document.getElementById('showingCategories').innerHTML = questionsCont;
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+function checkingAnswerTF() {
+  console.log('Probando verdadero falso');
+  /*
+  let btnTrue = document.getElementById('True');
+  let btnFalse = document.getElementById('False');
+  let trueAttribute = btnTrue.getAttribute('id');
+  // console.log(trueAttribute);
+  let falseAttribute = btnFalse.getAttribute('id');
+  // console.log(falseAttribute);
+
+if (trueAttribute === correctAnswer && falseAttribute !== correctAnswer) {
+  correctAnswersCounter++;
+  i++
+} if (falseAttribute === correctAnswer && trueAttribute !== correctAnswer) {
+  correctAnswersCounter ++;
+  i++
+  }
+  document.getElementById('pointsCounter').innerHTML = correctAnswersCounter;
+  */
+}
+
+function multipleChoice() {
+  fetch(`https://opentdb.com/api.php?amount=10&type=multiple`).
+    then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      let results = data.results;
+      let i = 0;
+      let question = results[i].question;
+      let category = results[i].category;
+      let difficulty = results[i].difficulty;
+      let fourAnswers = [];
+      correctAnswer = results[i].correct_answer;
+      let wrongAnswers = results[i].incorrect_answers;
+      // llevo alternativas incorrectas a un nuevo array
+      fourAnswers = wrongAnswers;
+      // agrego la alternativa correcta
+      fourAnswers.push(correctAnswer);
+      // desordeno las alternativas
+      fourAnswers.sort();
+      console.log(fourAnswers);
+      correctAnswersCounter = 0;
+
+      let questionsCont = `<div class="col s12 m8 offset-m2 center-align">
+        <div class="card grey darken-1">
+          <div class="card-content white-text">
+            <span class="card-title">
+              <span class="pink-text text-accent-1">${category}</span> |
+              <span class="teal-text text-lighten-1">${difficulty}</span> |
+              Question number ${i + 1}
+            </span>
+            <p class="questions">${question}</p>
+          </div>
+          <div class="card-action">
+            <a class="waves-effect waves-light btn pink btnMultiAnswers" onclick="checkingAnswerOfMulti();">${fourAnswers[0]}</a>
+            <a class="waves-effect waves-light btn pink btnMultiAnswers" onclick="checkingAnswerOfMulti();">${fourAnswers[2]}</a>
+            <a class="waves-effect waves-light btn pink btnMultiAnswers" onclick="checkingAnswerOfMulti();">${fourAnswers[1]}</a>
+            <a class="waves-effect waves-light btn pink btnMultiAnswers" onclick="checkingAnswerOfMulti();">${fourAnswers[3]}</a>
+          </div>
+        </div>
+      </div>`;
+      document.getElementById('showingCategories').innerHTML = questionsCont;
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+function checkingAnswerOfMulti() {
+  console.log('Holiwi');
+}
+
+/*
+function getMusic() {
+}
+
+
+function getVideogames() {
+}
+
+function getAnime() {
+} */
